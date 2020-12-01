aa1b1c9f15643998cef27578916a937da07167b6
dmitriy <dmitriysynkov@gmail.com>
2020-12-01 01:31:14 -0500

    Day one Scala

--- src/main/scala/com/adventofcode/AOC2020/PuzzleSolutionsScala.scala
+++ src/main/scala/com/adventofcode/AOC2020/PuzzleSolutionsScala.scala
@@ -8,12 +8,14 @@ object PuzzleSolutionsScala {
   def main(args: Array[String]): Unit = {
     val p1lines = PuzzleHelpersScala.readLinesFromInputFile(PUZZLE_1_INPUT)
 
-    println(f"Day #1 part #1: ${day1part1(p1lines)}")
-    println(f"Day #1 part #2: ${day1part2(p1lines)}")
+    println(f"Day #1 part #1: ${day1(p1lines, 2)}")
+    println(f"Day #1 part #2: ${day1(p1lines, 3)}")
 
   }
 
-  def day1part1(input: Array[String]): Unit = {}
-
-  def day1part2(input: Array[String]): Unit = {}
+  def day1(input: Array[String], combinations: Int): Int = {
+    input.map(_.toInt).combinations(combinations)
+      .filter(x => x.sum == 2020)
+      .toList.head.product
+  }
 }
